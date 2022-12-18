9689287a84d4b65376af7617207bd84323e3c941
Tuan Anh <tuanh061020@gmail.com>
2022-12-18 15:47:03 +0700

    Commit register

--- model/user.php
+++ model/user.php
@@ -4,7 +4,7 @@ class User extends Db
     public function checkLogin($username,$password){
         $sql = self::$connection->prepare("SELECT * FROM user 
         WHERE `username` = ? AND `password` = ?");
-        $password = md5($password);
+        //$password = md5($password);
         $sql->bind_param("ss", $username, $password);
         $sql->execute(); //return an object
         $items = array();
@@ -23,5 +23,50 @@ class User extends Db
         $items = array();
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
         return $items; //return an array
+    }
+    public function checkRegister($name,$phone,$email,$username,$password){
+        $sql = self::$connection->prepare("SELECT * FROM user 
+        WHERE `username` = ? AND `password` = ?");
+        //$password = md5($password);
+        $sql->bind_param("ss", $username, $password);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->num_rows;
+        if($items == 1){
+            return true;
+        }
+        else {
+            return false;
+        }
+
+        
+    }
+    public function checkEmailExists($email){
+        $sql = self::$connection->prepare("SELECT `id` FROM user 
+        WHERE `email` = ?");
+        $sql->bind_param("s", $email);
+        $sql->execute();
+        $items = array();
+        $items = $sql->get_result()->num_rows;
+        if($items == 1){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public function checkUsernameExists($username){
+        $sql = self::$connection->prepare("SELECT `id` FROM user 
+        WHERE `username` = ?");
+        $sql->bind_param("s", $username);
+        $sql->execute();
+        $items = array();
+        $items = $sql->get_result()->num_rows;
+        if($items == 1){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
